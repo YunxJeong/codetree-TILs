1c031946f0c09e3802008287f382a056ba5b7152
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-09 20:47:58 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240509/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
+++ 240509/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a, b;
+    double a, b;
 
     cin >> a >> b;
 
-    cout << pixed;
+    cout << fixed;
     cout.precision(2);
 
     cout << a + b;
