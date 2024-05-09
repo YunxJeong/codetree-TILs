24bae310e4d5d5c7c3558cdcf77b442225765845
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-09 20:47:07 +0900

    Title: 실수 입력받아 계산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240509/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << pixed;
+    cout.precision(2);
+
+    cout << a + b;
+    return 0;
+}
