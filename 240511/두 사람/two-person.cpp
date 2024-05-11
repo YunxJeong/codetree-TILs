bc7de3b38054bdf043c9b7c4dc00657206d1d00d
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-11 13:52:21 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240511/두 사람/two-person.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, c;
+    char b, d;
+
+    cin>>a>>b>>c>>d;
+
+    if(a>=19&&b=='M'||c>=19&&d=='M') cout << 1;
+    else 0;
+    return 0;
+}
