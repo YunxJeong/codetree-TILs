9cff2332a8e697412e783f14fdcc6be675103872
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-11 11:59:05 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240511/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+
+    cin >> n;
+
+    if(n==1) cout << "John";
+    else if(n==2) cout << "Tom";
+    else if(n==3) cout << "Paul";
+    else cout << "Vacnacy";
+    
+    return 0;
+}
