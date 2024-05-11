abb0fe50ca7fa4e8b5776b6fd834dd6801a2cd46
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-11 14:27:31 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240511/코로나 메뉴얼/covid-manual.cpp
+++ 240511/코로나 메뉴얼/covid-manual.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a, b, c, cnt = 0;
+    int a, b, c;
     char ca, cb, cc;
 
     cin >> ca >> a >> cb >> b >> cc >> c;
 
     if(ca == 'Y' && a >= 37) 
     {
-        if((cb == 'Y' && c >= 37)||(cc == 'Y' && c >= 37)) cout << "E";
+        if((cb == 'Y' && b >= 37)||(cc == 'Y' && c >= 37)) cout << "E";
         else cout << "N";
     }
     else
     {
-        if((cb == 'Y' && c >= 37)&&(cc == 'Y' && c >= 37)) cout << "E";
+        if((cb == 'Y' && b >= 37)&&(cc == 'Y' && c >= 37)) cout << "E";
         else cout << "N";
     }
 
