c9e0a12d3cb1cbde414014c898ba66f17567c729
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-11 13:18:13 +0900

    Title: 숫자의 계절은, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240511/숫자의 계절은/season-of-num.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m;
+
+    cin >> m;
+
+    if(n>=3&&n<=5) cout <<"Spring";
+    else if(n>=6&&n<=8) cout << "Summer";
+    else if(n>=9&&n<=11) cout << "Fall";
+    else cout << "Winter";
+    
+    return 0;
+}
