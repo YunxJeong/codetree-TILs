3495db19bdc509ff5b33724ff4d6f54d188b192c
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-12 21:50:32 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240512/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int sum = 0, n;
+
+    for(int i = 0; i < 10; i++)
+    {
+        cin >> n;
+        sum += n;
+    }
+
+    cout << sum;
+
+    return 0;
+}
