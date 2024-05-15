ca9e65f4405bbf5410143cab088ad4ecce8e68ee
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-15 18:03:44 +0900

    Title: 지그재그로 숫자 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240515/지그재그로 숫자 채우기/zigzag-numbering.cpp
+++ 240515/지그재그로 숫자 채우기/zigzag-numbering.cpp
@@ -7,26 +7,20 @@ int main() {
 
     cin >> n >> m;
 
-    int a[n][m] = {};
+    int a[100][100];
 
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < m; i++)
     {
-        if(i % 2 == 0)
+        for(int j = 0; j < n; j++)
+        {
+            if(i % 2 == 0)
             {
-                for(int j = 0; j < m; j++)
-                {
-                    a[j][i] = num;
-                    num++;
-                }
+                a[j][i] = num;
             }
-        else
-            {
-                for(int k = m - 1; k >= 0; k--)
-                {
-                    a[k][i] = num;
-                    num++;
-                }
-            }
+            else
+                a[n - j - 1][i] = num;
+        }
+        num++;
     }
 
     for(int i = 0; i < n; i++)
