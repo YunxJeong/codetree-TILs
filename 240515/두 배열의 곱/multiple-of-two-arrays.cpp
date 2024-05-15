02fd34b2e02e796886b1c8486e1e833c9d37f932
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-15 17:21:30 +0900

    Title: 두 배열의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240515/두 배열의 곱/multiple-of-two-arrays.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a[3][3], b[3][3];
+
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            cin >> b[i][j];
+        }
+    }
+
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            cout << a[i][j] * b[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
