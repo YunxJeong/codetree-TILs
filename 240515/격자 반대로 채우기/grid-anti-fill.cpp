316c736430322c1c44adb7e496c12c0b957b9836
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-15 20:44:32 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240515/격자 반대로 채우기/grid-anti-fill.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+
+    cin >> n;
+
+    int a[n][n], num = 1;
+
+    for(int i = n - 1; i >= 0; i--)
+    {
+        if(i % 2 != 0)
+        {
+            for(int j = n - 1; j >= 0; j--)
+            {
+                a[j][i] = num;
+                num++;   
+            }
+        }
+        else
+        {
+            for(int j = 0; j < n; j++)
+            {
+                a[j][i] =  num;
+                num++;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
