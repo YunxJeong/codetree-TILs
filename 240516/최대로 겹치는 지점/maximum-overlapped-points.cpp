2db2b033e55ac05cac2f5d775e3d72adc29c13f7
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-16 21:25:58 +0900

    Title: 최대로 겹치는 지점, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240516/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, arr[200] = {}, a, b;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a >> b;
+
+        for(int j = a; j <= b; j++)
+        {
+            arr[j]++;   
+        }
+    }
+
+    int max = 0;
+
+    for(int i = 0; i < 200; i++)
+    {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    cout << max;
+    
+    return 0;
+}
