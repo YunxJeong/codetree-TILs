3586f5c8ae971a93e88b2331fbb36b7fba8f6fb4
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-16 20:15:42 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240516/Date to Date/date-to-date.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int days(int m, int d)
+{
+    int sum = d;
+
+    int day[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    while(m > 0)
+    {
+        sum += day[m];
+        m--;  
+    }
+    
+    return sum;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m1, d1, m2, d2, a, b;
+
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    cout << days(m2, d2) - days(m1, d1);
+    
+    return 0;
+}
