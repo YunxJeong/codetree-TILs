b96c48b52c59161c6e09d1651022d788d9d856b5
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-16 21:06:24 +0900

    Title: 블럭쌓는 명령2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240516/블럭쌓는 명령2/block-stacking-commands2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, k, a, b, arr[100] = {};
+
+    cin >> n >> k;
+
+    for(int i = 0; i < k; i++)
+    {
+        cin >> a >> b;
+
+        for(int j = a; j <= b; j++)
+        {
+            arr[j] += 1;
+        }
+    }
+
+    int max = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    cout << max;
+
+    return 0;
+}
