e6444f395596d5a6eb779599aa0f4160207d630d
yuu._.uun <82822056+YunxJeong@users.noreply.github.com>
2024-05-21 21:22:45 +0900

    Title: 문자에 따른 명령 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240521/문자에 따른 명령 2/text-based-commands2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    int x = 0, y = 0, r = 3;
+    int dx[4] = {1, 0, -1, 0}, dy[y] = {0, -1, 0, 1};
+
+    cin >> str;
+
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(str[i] == 'L')
+        {
+            r = (r + 3) % 4;
+        }
+        else if(str[i] == 'R')
+        {
+            r = (r + 1) % 4;
+        }
+        else
+        {
+            x += dx[r];
+            y += dy[r];
+        }
+    }
+
+    cout << x << " " << y;
+
+    return 0;
+}
